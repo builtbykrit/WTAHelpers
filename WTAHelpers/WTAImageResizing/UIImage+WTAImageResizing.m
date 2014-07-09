0207288e7c6a933db9fc6d8192b9e7f626cd4075
eriklamanna <erik.lamanna@willowtreeapps.com>
2014-07-09 15:00:00 -0400

    Add WTAImageResizing category that supports stretching an image but preserving the center (the opposite of resizable image with cap insets).

---
+++ WTAHelpers/WTAImageResizing/UIImage+WTAImageResizing.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+WTAImageResizing.m
+//  WTAHelpers
+//
+//  Created by Erik LaManna on 7/9/14.
+//  Copyright (c) 2014 WillowTree Apps, Inc. All rights reserved.
+//
+
+#import "UIImage+WTAImageResizing.h"
+
+@implementation UIImage (WTAImageResizing)
+
+@end
---
+++ WTAHelpersDemo/Classes/WTAImageResizingDemo/WTAImageResizingViewController.h
@@ -0,0 +1,13 @@
+//
+//  WTAImageResizingViewController.h
+//  WTAHelpers
+//
+//  Created by Erik LaManna on 7/9/14.
+//  Copyright (c) 2014 WillowTree Apps, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WTAImageResizingViewController : UIViewController
+
+@end
