cc1e04c6b9351bd76322a44aff1e00bccdc54605
Robert Thompson <robert.thompson@willowtreeapps.com>
2015-08-20 13:44:59 -0400

    Add comments and documentation

--- WTAHelpers/WTASwiftHelpers/UIControl+ActionBinding.swift
+++ WTAHelpers/WTASwiftHelpers/UIControl+ActionBinding.swift
@@ -8,41 +8,77 @@
 
 import UIKit
 
-private class ActionBinding<T: UIControl>
+private class ActionBinding<T: UIControl>: NSObject
 {
-    weak var control: T?
-    let boundFunction: (T) -> Void
-    let action: UIControlEvents
+    private weak var control: T?
+    private let action: (T) -> Void
+    private let event: UIControlEvents
     
-    init(control: T, action: UIControlEvents, boundFunction: (T) -> Void)
+    /**
+    Create a new ActionBinding
+    
+    - parameter control:       The control who's action you wish to bind
+    - parameter event:        The UIControlEvents you want to bind to
+    - parameter action: The closure that will be executed when the above event happens
+    
+    - returns: The ActionBinding object
+    */
+    private init(control: T, event: UIControlEvents, action: (T) -> Void)
     {
         self.control = control
-        self.boundFunction = boundFunction
         self.action = action
+        self.event = event
         
-        control.addTarget(self, action: "actionHappened:", forControlEvents: action)
+        super.init()
+        
+        control.addTarget(self, action: "actionHappened:", forControlEvents: event)
     }
+   
+    /**
+    The method called when the bound action fires
     
-    dynamic func actionHappened(sender: UIControl)
+    - parameter sender: This is sent by the system when the UIControl's action fires.
+    It's defined as a UIControl here and not T because this method must be visible to Objective-C
+    */
+    private dynamic func actionHappened(sender: UIControl)
     {
-        boundFunction(sender as! T)
+        if let control = control
+        {
+            action(control)
+        }
     }
     
     deinit {
-        control?.removeTarget(self, action: "actionHappened:", forControlEvents: action)
+        control?.removeTarget(self, action: "actionHappened:", forControlEvents: event)
     }
 }
 
-private func createBinding<T: UIControl>(control: T, action: UIControlEvents, function: (T) -> Void) -> ActionBinding<T>
+/**
+This factory function is required due to a compiler bug with calling ActionBinding<Self>(…) in 
+the protocol extension.
+
+- parameter control:  The control who's action you wish to bind
+- parameter action:  The UIControlEvents you want to bind to
+- parameter function: The closure that will be executed when the above action happens
+
+- returns: The ActionBinding object
+*/
+private func createBinding<T: UIControl>(control: T, event: UIControlEvents, action: (T) -> Void) -> ActionBinding<T>
 {
-    return ActionBinding<T>(control: control, action: action, boundFunction: function)
+    return ActionBinding<T>(control: control, event: event, action: action)
 }
 
-public protocol UIControlBindable: AnyObject
+/**
+    This protocol specifies the onEvent method, which is actually implemented in the private
+    protocol extension
+*/
+public protocol UIControlActionBindable: NSObjectProtocol
 {
+    func onEvent(event: UIControlEvents, doAction action: Action) -> Void
 }
 
-public extension UIControlBindable where Self: UIControl
+// MARK: - UIControlActionBindable extension
+public extension UIControlActionBindable where Self: UIControl
 {
     private var bindings: [ActionBinding<Self>] {
         get {
@@ -51,15 +87,28 @@ public extension UIControlBindable where Self: UIControl
         }
     }
     
-    public func bindToEvent(event: UIControlEvents, boundFunc: (Self) -> Void) {
-        let binding = createBinding(self, action: event, function: boundFunc)
+    /// A closure which takes a parameter of type whatever this UIControl actually is
+    public typealias Action = Self -> Void
+    
+    /**
+    This method binds the provided closure to this control, fired when the given events occur
+    
+    - parameter event:     A mask of events you want to bind an action to
+    - parameter doAction: The closure that fires when the above events occur
+    */
+    public func onEvent(event: UIControlEvents, doAction action: Action) {
+        let binding = createBinding(self, event: event, action: action)
         let bindings = self.bindings + [binding]
         objc_setAssociatedObject(self, &AssociatedKeys.Bindings, bindings, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
     }
 }
 
-extension UIControl: UIControlBindable {
-    private struct AssociatedKeys {
-        static var Bindings = "Bindings"
-    }
+/**
+    Provides the static pointer that objc_setAssociatedObject and objc_getAssociatedObject need for a key.
+*/
+private struct AssociatedKeys {
+    static var Bindings = "Bindings"
 }
+
+extension UIControl: UIControlActionBindable { }
+
